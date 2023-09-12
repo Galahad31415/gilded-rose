99553b37f9af0c011951154220dc06e8611c4cbf
Galahad31415 <martin.reboutier@icloud.com>
2023-09-12 16:20:23 +0200

    fix: refactor coadebase

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
-
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
+  constructor(public name: string, public sellIn: number, public quality: number) {}
 }
 
 export class GildedRose {
@@ -18,52 +10,80 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+    for (const item of this.items) {
+      const updater = ItemUpdaterFactory.create(item);
+      updater.update();
     }
-
     return this.items;
+  }
+}
+
+abstract class ItemUpdater {
+  constructor(protected item: Item) {}
+
+  abstract update(): void;
+
+  increaseQuality(amount: number) {
+    this.item.quality = Math.min(this.item.quality + amount, 50);
+  }
+
+  decreaseQuality(amount: number) {
+    this.item.quality = Math.max(this.item.quality - amount, 0);
+  }
+
+  decreaseSellIn() {
+    this.item.sellIn -= 1;
+  }
+}
+
+class ItemUpdaterFactory {
+  static create(item: Item): ItemUpdater {
+    switch (item.name) {
+      case 'Aged Brie':
+        return new AgedBrieUpdater(item);
+      case 'Sulfuras, Hand of Ragnaros':
+        return new SulfurasUpdater(item);
+      case 'Backstage passes to a TAFKAL80ETC concert':
+        return new BackstagePassUpdater(item);
+      default:
+        return new NormalItemUpdater(item);
+    }
+  }
+}
+
+class AgedBrieUpdater extends ItemUpdater {
+  update() {
+    this.increaseQuality(1);
+    this.decreaseSellIn();
+    if (this.item.sellIn < 0) {
+      this.increaseQuality(1);
+    }
+  }
+}
+
+class SulfurasUpdater extends ItemUpdater {
+  update() {
+    
+  }
+}
+
+class BackstagePassUpdater extends ItemUpdater {
+  update() {
+    if (this.item.sellIn > 10) this.increaseQuality(1);
+    else if (this.item.sellIn > 5) this.increaseQuality(2);
+    else if (this.item.sellIn > 0) this.increaseQuality(3);
+    else this.item.quality = 0;
+
+    this.decreaseSellIn();
+  }
+}
+
+class NormalItemUpdater extends ItemUpdater {
+  update() {
+    this.decreaseQuality(1);
+    this.decreaseSellIn();
+    if (this.item.sellIn < 0) {
+      this.decreaseQuality(1);
+    }
   }
 }
